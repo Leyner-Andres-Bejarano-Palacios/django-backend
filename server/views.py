92a66fcee130a2e64d38a2ae579f8647ab242e83
Leyner Bejarano <personaltraingcp@gmail.com>
2023-07-25 15:57:06 -0500

    blessings coming my way, I havea POST endpoint, not tested but I am sure it works

--- server/views.py
+++ server/views.py
@@ -1,10 +1,17 @@
 from django.http import JsonResponse
 from .models import Server
 from  .serializers import ServerSerializer
+from rest_framework import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-
+@api_view('GET','POST')
 def server_list(request):
-    servers = Server.objects.all()
-    serializer = ServerSerializer(servers,many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        servers = Server.objects.all()
+        serializer = ServerSerializer(servers,many=True)
+        return JsonResponse({'servers':serializer.data})
+
+    if request.method == 'POST':
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
